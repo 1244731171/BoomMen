a7397100ce544721e28eb2a29251d41ec799bc1e
CJuNing <575960273@qq.com>
2019-01-06 22:05:55 +0800

    qqqqqqqqqqqqqq

--- BoomMan/downloadImg/k886/imageCollecter.1.js
+++ BoomMan/downloadImg/k886/imageCollecter.1.js
@@ -11,9 +11,13 @@ let imageJsonPath = './' + title + '/image.json';
 let callbacks = [];
 let chapterData;
 let imageData = [];
+let _imageData = {};
+let chapterNumbers = 0;
+
+let errors = [];
 
 let index = 1;
-let data;
+let data = {};
 
 let getChapterData = () => {
     logger.log('__imageCollecter__: >>>>>> START! <<<<<<');
@@ -21,8 +25,9 @@ let getChapterData = () => {
         logger.log('__imageCollecter__: image.json exist! >>>> ', imageJsonPath);
         imageData = fs.readFileSync(imageJsonPath, 'utf8');
         imageData = JSON.parse(imageData);
-        index = imageData.length + 1;
-        logger.log('__imageCollecter__: image.json length >>>> ', index - 1);
+        imageData.forEach(ele => {
+            _imageData[ele.index] = ele;
+        });
     }
     // 异步读取
     fs.readFile(chapterJsonPath, function (err, data) {
@@ -30,45 +35,82 @@ let getChapterData = () => {
             return logger.error(err);
         }
         chapterData = JSON.parse(data);
-        chapterData.splice(0, index - 1);
-        logger.log('__imageCollecter__: rest chapterData length >>>> ', chapterData.length);
+        chapterNumbers = chapterData.length;
+        // chapterData.splice(0, index - 1);
+        logger.log('__imageCollecter__: all chapterData length >>>> ', chapterData.length);
         getImageInfo();
     });
 }
-
+// 第(\d){1,2}頁</option>
 let getImageInfo = () => {
     concatImageData();
     if (chapterData.length <= 0) {
-        collectEnd();
         return;
     }
     let mainUrl = chapterData.splice(0, 1)[0];
+    if (_imageData[index] !== undefined) {
+        logger.log('__imageCollecter__: image data exist! index >>>> %s, url >>>> %s', index, mainUrl);
+        index++;
+        data = {};
+        getImageInfo();
+        return;
+    }
+    logger.log('__imageCollecter__: index >>>> %s <<<<, url >>>> %s', index, mainUrl);
     data = {};
     data['index'] = index++;
     data['url'] = mainUrl;
     data['data'] = [];
-    logger.log('__imageCollecter__: index >>>> %s, url >>>> %s', index, mainUrl);
     getImagePageInfo(mainUrl, 1);
 }
 
 let concatImageData = () => {
-    if(data){
-        imageData.push(data);
-        writeJson();
+    if (data['data'] && data['data'].length) {
+        data['realMax'] = data['data'].length.toString();
+        logger.log('__imageCollecter__: index >>>> %s <<<<, max >>>> %s, realMax >>>> %s', data['index'], data['max'], data['realMax']);
+        if (data.max !== data.realMax) {
+            logger.log('__imageCollecter__: index >>>> %s <<<<, max !== realMax >>>> BAN!', data['index']);
+            errors.push(JSON.stringify(data));
+            return;
+        }
+        _imageData[data.index] = data;
+        let _i = 0;
+        imageData = [];
+        while (_i++ < chapterNumbers) {
+            if (_imageData[_i]) {
+                imageData.push(_imageData[_i]);
+            }
+        }
+        writeJson(!!!chapterData.length);
+    } else {
+        // logger.log('__imageCollecter__: current data ERROR! >>>> ', data);
     }
 }
 
 let getImagePageInfo = (url, pageIndex) => {
+    if (data['max'] && pageIndex > data['max']) {
+        getImageInfo();
+        return;
+    }
+
     let mainUrl = url + '-p-' + pageIndex;
-    logger.log('url >>>> ', mainUrl);
+
+    logger.log('__imageCollecter__: pageUrl >>>> ', mainUrl);
     request(mainUrl, { json: false }, (err, res, body) => {
-        if (err) { return logger.log(err); }
+        if (err) {
+            logger.log(err);
+        }
+
+        if (pageIndex === 1) {
+            let pages = body.match(/第(\d){1,2}頁\<\/option\>/g);
+            data['max'] = pages.pop().replace('第', '').replace('頁</option>', '');
+        }
+
         let imageUrl = body.match(/https\:\/\/([a-z]|[A-Z]|\.|[0-9]|\/)*.jpg/g);
         if (!imageUrl || !imageUrl.length) {
             getImageInfo();
         } else {
             imageUrl = imageUrl[0];
-            logger.log(imageUrl);
+            logger.log('__imageCollecter__: imageUrl >>>> ', imageUrl);
             if (data['data'].indexOf(imageUrl) === -1) {
                 data['data'].push(imageUrl);
                 getImagePageInfo(url, pageIndex + 1);
@@ -79,24 +121,31 @@ let getImagePageInfo = (url, pageIndex) => {
     });
 }
 
-let writeJson = () => {
+let writeJson = (isFinish) => {
+    let str = JSON.stringify(imageData);
     fs.open(imageJsonPath, 'w', (err, fd) => {
         if (err) {
             logger.log('__imageCollecter__: ERROR ' + err);
         }
-        let str = JSON.stringify(imageData);
-        logger.log('__imageCollecter__: write image json');
+        if (isFinish) {
+            logger.log('__imageCollecter__: collect SUCCESSFUL!, write image.json');
+        }
         fs.writeFile(fd, str, (err) => {
             if (err) {
                 logger.log('__imageCollecter__: ERROR ' + err);
             }
             fs.close(fd, (err) => {
-                logger.log(err ? ('__imageCollecter__: ERROR ' + err) : '__imageCollecter__: SUCCESSFUL!');
-                callbacks.forEach(element => {
-                    if (typeof element === 'function') {
-                        element();
+                if (err) {
+                    logger.log('__imageCollecter__: ERROR ' + err);
+                }
+                if (isFinish) {
+                    if (errors.length > 0) {
+                        logger.log('__imageCollecter__: errors data >>>> ', errors);
+                        return;
                     }
-                });
+                    logger.log('__imageCollecter__: write SUCCESSFUL!');
+                    collectEnd();
+                }
             });
         });
     });
--- BoomMan/downloadImg/k886/main.js
+++ BoomMan/downloadImg/k886/main.js
@@ -1,17 +1,23 @@
 const chapterCollecter = require('./chapterCollecter');
-const imageCollecter = require('./imageCollecter');
+const imageCollecter = require('./imageCollecter.1');
 const imageDownloader = require('./imageDownloader');
 const imageConcater = require('./imageConcater');
 const htmlCreater = require('./htmlCreater');
 const logger = require('./log');
 
 // let mainUrl = 'https://www.k886.net/index-comic-name-%E4%B8%8D%E5%80%AB%E9%A7%95%E8%A8%93%E7%8F%AD-id-35958';
-let mainUrl = 'https://www.k886.net/index-comic-name-%E8%A3%BD%E4%BD%9C%E4%BA%BA%E7%B7%B4%E7%BF%92%E7%94%9F-id-36573';
+// let mainUrl = 'https://www.k886.net/index-comic-name-%E8%A3%BD%E4%BD%9C%E4%BA%BA%E7%B7%B4%E7%BF%92%E7%94%9F-id-36573';
+let mainUrl = 'https://www.k886.net/index-comic-name-%E5%A4%A9%E4%BD%BF%E7%9A%84%E7%9C%BC%E6%B7%9A-id-36132';
 
+// let mainUrl = 'https://www.k886.net/index-comic-name-%E7%8C%9C%E4%B8%8D%E9%80%8F%E7%9A%84%E5%BF%83-id-36242';
+// https://www.k886.net/index-comic-name-%E9%82%A3%E8%A3%A1%E7%9A%84%E9%A6%99%E6%B0%A3-id-36125
+// let mainUrl = 'https://www.k886.net/index-comic-name-MAD%E5%B0%8F%E5%A7%90%E8%88%87%E5%8F%B8%E6%A9%9F-id-36190';
 // let mainUrl = 'https://www.k886.net/index-comic-name-%E6%B7%AB%E6%96%B0%E5%B0%8F%E5%A5%97%E6%88%BF-id-36330';
 // let mainUrl = 'https://www.k886.net/index-comic-name-SuperDick-id-36391';
 // let mainUrl = 'https://www.k886.net/index-comic-name-%E5%82%80%E5%84%A1-id-36455';
 // let mainUrl = 'https://www.k886.net/index-comic-name-%E5%A5%B9%E7%9A%84%E9%AB%98%E8%B7%9F%E9%9E%8B-id-36222';
+// let mainUrl = 'https://www.k886.net/index-comic-name-DarkSea-id-36126';
+
 let title = '';
 let collectChapter = () => {
     logger.log(' >>>>>> COLLECTCHAPTER <<<<<< ');
@@ -32,7 +38,7 @@ let collectImage = () => {
 let createChapterHtml = () => {
     logger.log(' >>>>>> CTEATECHAPTERHTML <<<<<< ');
     htmlCreater.setTitle(title);
-    // htmlCreater.setCallBack(downloadImages);
+    htmlCreater.setCallBack(downloadImages);
     htmlCreater.start();
 }
 
