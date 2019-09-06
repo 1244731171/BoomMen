34740dac115d8c46b2867a9f09c3d394231ba1fa
CJuNing <575960273@qq.com>
2019-09-06 21:38:27 +0800

    update

--- game/molly/base.js
+++ game/molly/base.js
@@ -2,9 +2,10 @@ let getUrlParam = (key) => {
     let url = window.location.href.split("?")[1] || "";
     let params = url.split("&");
     let val = "";
-    for (let k in params) {
-        if (k === key) {
-            val = params[k];
+    for (let k of params) {
+        let kv = k.split("=");
+        if (kv[0] === key) {
+            val = kv[1];
             break;
         }
     }
