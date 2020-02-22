be4c04ddbc95b4b0d96911ae36368fe179e37410
CJuNing <575960273@qq.com>
2020-02-22 16:24:03 +0800

    vue trip static file

--- vueProject/trip/trip/config/index.js
+++ vueProject/trip/trip/config/index.js
@@ -38,10 +38,11 @@ module.exports = {
 
     build: {
         // Template for index.html
-        index: path.resolve(__dirname, '../dist/index.html'),
+        // index: path.resolve(__dirname, '../dist/index.html'),
+        index: path.resolve(__dirname, '../../../../static/trip/trip.html'),
 
         // Paths
-        assetsRoot: path.resolve(__dirname, '../dist'),
+        assetsRoot: path.resolve(__dirname, '../../../../static/trip'),
         assetsSubDirectory: 'static',
         assetsPublicPath: '/',
 
